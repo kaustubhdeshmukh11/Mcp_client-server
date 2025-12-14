64239e8cd9d7133e80b76f477780ec50d102851a
Kaustubh Deshmukh <kaustubhsdeshmukh45@gmail.com>
2025-12-15 01:12:58 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from mcp!")
+import random 
+from fastmcp import FastMCP 
 
+mcp=FastMCP(name="Demo seerver")
 
-if __name__ == "__main__":
-    main()
+@mcp.tool
+def roll_dice(n : int =1)->list[int]:
+    """Roll n 6-sidedd dice and return the result"""
+    return [random.randint(1,6) for _ in range(n)]
+
+if __name__ =="__main__":
+    mcp.run()
+
